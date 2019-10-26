7c86f01c5a115eb5192a456746974d8e363dc655
unknown <827797174@qq.com>
2019-10-26 10:57:55 +0800

    first commit

--- app/src/main/java/cn/johnyu/day03/MainActivity.java
+++ app/src/main/java/cn/johnyu/day03/MainActivity.java
@@ -1,11 +1,15 @@
 package cn.johnyu.day03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -14,95 +18,148 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     private Uri imgUri; //记录拍照后的照片文件的地址(临时文件)
     private ImageView imgView;//用于查看照片的view
+    private String uploadFileName;
+    private byte[] fileBuf;
+    private String uploadUrl = "http://172.20.10.3:8000/upload";
+    Button select;
+    Button photo;
+    Button getPrem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imgView=findViewById(R.id.imageView);
-    }
-
-    public void baidu(View view) {
-
-        new Thread(){
+        imgView = findViewById(R.id.imageView);
+        getPrem = findViewById(R.id.getPrem);
+        select = (Button) findViewById(R.id.select);
+        photo = (Button) findViewById(R.id.photo);
+        getPrem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                }
+            }
+        });
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outImg = new File(getExternalCacheDir(), "temp.jpg");
                 try {
-                    OkHttpClient client=new OkHttpClient();
-                    Request request=new Request
-                            .Builder()
-                            .url("http://www.baidu.com")
-                            .get()
-                            .build();
-                    Response response= client.newCall(request).execute();
-                    response.headers();
-                    String s=response.body().string();
-                    Log.i("baidu",s);
+                    if (outImg.exists()) {
+                        outImg.delete();
+                    }
+                    outImg.createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                //复杂的Uri创建方式
+                if (Build.VERSION.SDK_INT >= 24)
+                    //这是Android 7后，更加安全的获取文件uri的方式（需要配合Provider,在Manifest.xml中加以配置）
+                    imgUri = FileProvider.getUriForFile(MainActivity.this, "cn.john.app1.fileprovider", outImg);
+                else
+                    imgUri = Uri.fromFile(outImg);
+
+                //利用actionName和Extra,启动《相机Activity》
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                startActivityForResult(intent, 1);
+
             }
-        }.start();
+        });
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] permissions = new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                };
+                //进行sdcard的读写请求
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+                } else {
+                    openGallery(); //打开相册，进行选择
+                }
+            }
+        });
     }
-
-    public void getPrem(View view) {
-        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        }
+    private void openGallery() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, 2);
     }
-
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode){
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+
             case 1:
-                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    //得到了用户的允许
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery();
+                } else {
+                    Toast.makeText(this, "读相册的操作被拒绝", Toast.LENGTH_LONG).show();
                 }
-                else{
-                    //用户拒绝
-                }
+                break;
+            default:
+                break;
         }
     }
 
-    public void photo(View view) throws Exception{
+    /*public void getPrem(View view) {
+           if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+               requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+           }
+       }
+   */
+
+
+
+   /* public void photo(View view) throws Exception {
 
         //删除并创建临时文件，用于保存拍照后的照片
         //android 6以后，写Sdcard是危险权限，需要运行时申请，但此处使用的是"关联目录"，无需！
-        File outImg=new File(getExternalCacheDir(),"temp.jpg");
-        if(outImg.exists()) outImg.delete();
+        File outImg = new File(getExternalCacheDir(), "temp.jpg");
+        if (outImg.exists()) outImg.delete();
         outImg.createNewFile();
 
         //复杂的Uri创建方式
-        if(Build.VERSION.SDK_INT>=24)
+        if (Build.VERSION.SDK_INT >= 24)
             //这是Android 7后，更加安全的获取文件uri的方式（需要配合Provider,在Manifest.xml中加以配置）
-            imgUri= FileProvider.getUriForFile(this,"cn.john.app1.fileprovider",outImg);
+            imgUri = FileProvider.getUriForFile(this, "cn.john.app1.fileprovider", outImg);
         else
-            imgUri=Uri.fromFile(outImg);
+            imgUri = Uri.fromFile(outImg);
 
         //利用actionName和Extra,启动《相机Activity》
-        Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,imgUri);
-        startActivityForResult(intent,1);
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+        startActivityForResult(intent, 1);
 
     }
+*/
 
-    @Override
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case 1:
                 //此时，相机拍照完毕
@@ -116,6 +173,92 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
+                break;
+            case 2:
+                handleSelect(data);
+                break;
         }
     }
+
+
+    private void handleSelect(Intent intent) {
+        Cursor cursor = null;
+        Uri uri = intent.getData();
+        cursor = getContentResolver().query(uri, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME);
+            uploadFileName = cursor.getString(columnIndex);
+        }
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            fileBuf = convertToBytes(inputStream);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(fileBuf, 0, fileBuf.length);
+            imgView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        cursor.close();
+    }
+
+    //文件上传的处理
+    public void upload(View view) {
+        new Thread() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                //上传文件域的请求体部分
+                RequestBody formBody = RequestBody
+                        .create(fileBuf, MediaType.parse("image/jpeg"));
+                //整个上传的请求体部分（普通表单+文件上传域）
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("title", "Square Logo")
+                        //filename:avatar,originname:abc.jpg
+                        .addFormDataPart("avatar", uploadFileName, formBody)
+                        .build();
+                Request request = new Request.Builder()
+                        .url(uploadUrl)
+                        .post(requestBody)
+                        .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    Log.i("数据", response.body().string() + "....");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
+    }
+
+    private byte[] convertToBytes(InputStream inputStream) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        int len = 0;
+        while ((len = inputStream.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        out.close();
+        inputStream.close();
+        return out.toByteArray();
+    }
+
+  /*  public void onClick(View v) {
+        {
+
+
+            String[] permissions = new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+            //进行sdcard的读写请求
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+            } else {
+                openGallery(); //打开相册，进行选择
+            }
+
+        }
+    }*/
 }
